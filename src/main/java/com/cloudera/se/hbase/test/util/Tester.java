b965f959376db7805a067449e5819e3daaa61964
jholoman <jeff.holoman@gmail.com>
2015-05-10 08:58:38 -0400

    Adding Batching

--- src/main/java/com/cloudera/se/hbase/test/util/Tester.java
+++ src/main/java/com/cloudera/se/hbase/test/util/Tester.java
@@ -35,7 +35,12 @@ public class Tester {
             System.out.println("---------");
             return;
         }
+        Configuration config = HBaseConfiguration.addHbaseResources(new Configuration());
+        HConnection connection = HConnectionManager.createConnection(config);
+
         long events = Long.parseLong(args[0]);
+        long batchSize = Long.parseLong(args[1]);
+        long batchCounter = 0;
 
         String now = sdf.format(new Date());
         List<Put> table1PutList = new ArrayList<Put>();
@@ -45,33 +50,41 @@ public class Tester {
         List<Put> table5PutList = new ArrayList<Put>();
 
         for (long nEvents = 0; nEvents < events; nEvents++) {
+            batchCounter++;
             Event event = new Event();
-            table1PutList.add(CEHUtils.getTable1Put(event));
-            table2PutList.add(CEHUtils.getTable2Put(event));
-            table3PutList.add(CEHUtils.getTable3Put(event));
-            table4PutList.add(CEHUtils.getTable4Put(event));
-            table5PutList.add(CEHUtils.getTable5Put(event));
+            if (batchCounter < batchSize && nEvents < events) {
+                table1PutList.add(CEHUtils.getTable1Put(event));
+                table2PutList.add(CEHUtils.getTable2Put(event));
+                table3PutList.add(CEHUtils.getTable3Put(event));
+                table4PutList.add(CEHUtils.getTable4Put(event));
+                table5PutList.add(CEHUtils.getTable5Put(event));
+            } else  { // do the batch
+                System.out.println("Starting");
+                long startTime = System.currentTimeMillis();
+                CEHUtils.insertToTable(connection,table1PutList, DataModelConsts.TABLE1);
+                CEHUtils.insertToTable(connection,table2PutList, DataModelConsts.TABLE2);
+                CEHUtils.insertToTable(connection,table3PutList, DataModelConsts.TABLE3);
+                CEHUtils.insertToTable(connection,table4PutList, DataModelConsts.TABLE4);
+                CEHUtils.insertToTable(connection,table5PutList, DataModelConsts.TABLE5);
+                long endTime = System.currentTimeMillis();
+                System.out.println("Ended");
+                System.out.println(endTime - startTime);
+                String then = sdf.format(new Date());
+                System.out.println(now);
+                System.out.println(then);
+
+                //cleanup
+                batchCounter = 0;
+                table1PutList.clear();
+                table2PutList.clear();
+                table3PutList.clear();
+                table4PutList.clear();
+                table5PutList.clear();
+            }
+        }
 
         }
-        Configuration config = HBaseConfiguration.addHbaseResources(new Configuration());
 
-        HConnection connection = HConnectionManager.createConnection(config);
-
-        System.out.println("Starting");
-        long startTime = System.currentTimeMillis();
-        CEHUtils.insertToTable(connection,table1PutList, DataModelConsts.TABLE1);
-        CEHUtils.insertToTable(connection,table2PutList, DataModelConsts.TABLE2);
-        CEHUtils.insertToTable(connection,table3PutList, DataModelConsts.TABLE3);
-        CEHUtils.insertToTable(connection,table4PutList, DataModelConsts.TABLE4);
-        CEHUtils.insertToTable(connection,table5PutList, DataModelConsts.TABLE5);
-        long endTime = System.currentTimeMillis();
-        System.out.println("Ended");
-        System.out.println(endTime - startTime);
-
-        String then = sdf.format(new Date());
-        System.out.println(now);
-        System.out.println(then);
-        }
 
 
     }
